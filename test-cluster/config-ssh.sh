2387a477bb4e8cb5cb40eb58a254b5d2a9093927
Wu, Xiaochang <xiaochang.wu@intel.com>
2021-04-01 08:32:20 +0800

    Add test-cluster

---
+++ test-cluster/config-ssh.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -x
+
+ssh-keygen -q -N "" -t rsa -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+echo "    StrictHostKeyChecking no                     " | sudo tee -a /etc/ssh/ssh_config
+sudo service ssh restart
+
+cat ~/.ssh/authorized_keys
+cat /etc/ssh/ssh_config
+cat /etc/ssh/sshd_config
+
+ls -ld ~/.ssh
+ls -l ~/.ssh
+
+# sudo systemctl restart sshd
+
+# ssh -vvv localhost
+ssh -vvv `hostname`
