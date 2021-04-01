2d92bb31e0149aa42690aff43ab66e8477f69039
Wu, Xiaochang <xiaochang.wu@intel.com>
2021-04-01 09:51:09 +0800

    nit

--- test-cluster/config-ssh.sh
+++ test-cluster/config-ssh.sh
@@ -3,26 +3,15 @@
 set -x
 
 ssh-keygen -q -N "" -t rsa -f ~/.ssh/id_rsa
-chmod 664 ~/.ssh/id_rsa.pub
-
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
-
-echo "    StrictHostKeyChecking no                     " | sudo tee -a /etc/ssh/ssh_config
-echo "RSAAuthentication yes" | sudo tee -a /etc/ssh/sshd_config
-# echo "StrictModes no" | sudo tee -a /etc/ssh/sshd_config
-sudo service ssh restart
-sudo systemctl restart ssh
-sudo systemctl restart sshd
-
-# cat ~/.ssh/authorized_keys
-# cat /etc/ssh/ssh_config
-# cat /etc/ssh/sshd_config
-
 ls -ld ~/.ssh
 ls -l ~/.ssh
 
-# sudo systemctl restart sshd
+echo "    StrictHostKeyChecking no                     " | sudo tee -a /etc/ssh/ssh_config
+echo "RSAAuthentication yes" | sudo tee -a /etc/ssh/sshd_config
+echo "StrictModes no" | sudo tee -a /etc/ssh/sshd_config
 
-# ssh -vvv localhost
+sudo systemctl restart ssh
+
 ssh -v `hostname`
